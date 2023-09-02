0d44ff1a79a28d52f09d3979de791d890f7c0823
Hyunjoon83 <141709404+Hyunjoon83@users.noreply.github.com>
2023-09-02 17:25:11 +0900

    Delete OOP directory

--- OOP/Lab10/src/lab10/Person.java
+++
@@ -1,6 +0,0 @@
-package lab10;
-
-public interface Person {
-	void control(Animal animal);
-	void showInfo();
-}
--- OOP/Lab13/src/Assignment/Observer.java
+++
@@ -1,5 +0,0 @@
-package Assignment;
-
-public interface Observer {
-	public abstract void update(NumberGenerator generator);
-}//update를 호출하는 interface
